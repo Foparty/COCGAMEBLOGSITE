97f95fc79efc5d34d6f3595f268a65e98eec4be4
Foparty <103907142+Foparty@users.noreply.github.com>
2023-03-21 11:26:00 +0100

    Nav and Layout done
    added nav and layout components + font

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './Nav.module.css';
+import { logo } from '../../../public';
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Nav = () => {
+	return (
+		<nav className={styles.mainnav}>
+			<div className={styles.navcontent}>
+				{/* empty div for maybe useranme on login or something like that */}
+				<Link href={'/'} className={styles.leftnav}>
+					Back Home
+				</Link>
+				<Link href={'/'}>
+					<Image
+						priority={true}
+						src={logo}
+						className={styles.logo}
+						height={80}
+						alt="Clash of Clans Logo with no background png"
+					/>
+				</Link>
+				<Link href={'/blog'}>Blog</Link>
+			</div>
+		</nav>
+	);
+};
+
+export default Nav;
