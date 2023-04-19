cb4be86181c63d571d276e5fa798a731772dfd43
Foparty <alfonsomartingarcia@gmail.com>
2023-04-19 23:37:01 +0200

    fixing sceneries

--- src/pages/wiki/sceneries/index.js
+++ src/pages/wiki/sceneries/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 // import { data } from '@/pages/wiki/sceneries/data.json';
 import styles from './Sceneries.module.css';
-import SceneryCard from '@/components/SceneryCard/SceneryCard';
+// import SceneryCard from '@/components/SceneryCard/SceneryCard';
 const Sceneries = () => {
 	return (
 		<main className={styles.main}>
-			{data.map((item) => {
+			{/* {data.map((item) => {
 				return <SceneryCard key={item.id} name={item.name} thumb={item.thumb} img={item.img} />;
-			})}
+			})} */}
 		</main>
 	);
 };
